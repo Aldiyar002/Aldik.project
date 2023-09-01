e2ebe7ed778db91318eee0850b955f912e558fea
Aldiyar002 <zhambylaldiyar@gmail.com>
2023-09-01 16:15:42 +0600

    Commit 1

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,23 @@
-console.log("Hello world!");
+// 1
+
+let films=['Naruto', 'Astral', 'Dorama', 'Forsaj']
+films.push('Iron Man');
+
+films.pop();
+console.log(films);
+
+// 2
+LetterFinder=()=>{
+    console.log('Word');
+    console.log('Match');
+}
+LetterFinder()
+
+// 3 tusinbedim
+
+// 4tusinbedim
+
+
+// 5
+const arr=[5, 6, 7, 8, 9];
+console.log(arr.reverse());
